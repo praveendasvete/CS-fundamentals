00ed7ba93b5f57f141c3e69cf5e36fda5d526000
acmsahyadritech <67265697+acmsahyadritech@users.noreply.github.com>
2021-10-03 00:22:19 +0530

    added stack

--- dsa2.cpp
+++ dsa2.cpp
@@ -1,25 +1,40 @@
-void printInOrder(node* root){
-    if(root==NULL){
-        return;
-    }
-    printInOrder(root->left);
-    cout<<root->data<<" ";
-    printInOrder(root->right);
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+void push(vector<int>&a){
+    a.push_back(10);
+    a.push_back(20);
 }
-void printPostOrder(node* root){
-    if(root==NULL){
-        return;
-    }
-    printPostOrder(root->left);
-    printPostOrder(root->right);
-    cout<<root->data<<" ";
+
+void top(vector<int>&a){
+    int k;
+    k = a[a.size()-1];
+    cout<<k<<endl;
 }
-void printPreOrder(node* root){
-    if(root==NULL){
-        return;
-    }
-    cout<<root->data<<" ";
-    printPostOrder(root->left);
-    printPostOrder(root->right);
+
+bool isEmpty(vector<int>&a){
+    return a.size()==0;
+}
+
+void pop(vector<int>&a)
+{
+    if(!isEmpty(a))
+        a.pop_back();
+}
+
+
+int main()
+{
+    vector<int>a;
     
+    push(a);
+    top(a);
+    pop(a);
+    top(a);
+    
+    
+    return 0;
+}
 }
