3677b373e16a5ba5fcd7084a0d52aecd5a09b8ef
GeethaTallur <72148952+GeethaTallur@users.noreply.github.com>
2021-10-03 00:52:44 +0530

    subset sum

--- Dsa.cpp
+++ Dsa.cpp
@@ -119,3 +119,24 @@ int main()
    
     return 0;
 }
+void subsets(char *a,int i,int j,char *temp){
+    if(a[i]=='\0'){
+        temp[j]='\0';
+        cout<<temp<<endl;
+        return;
+    }
+    
+    temp[j]=a[i];
+    subsets(a,i+1,j+1,temp);
+    subsets(a,i+1,j,temp);
+}
+
+int main()
+{
+    char a[100];
+    cin>>a;
+    char temp[100];
+    subsets(a,0,0,temp);
+
+    return 0;
+}
