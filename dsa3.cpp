9d4e4a4dcad7808b437d1b77a9e3e65c58cae10f
acmsahyadritech <67265697+acmsahyadritech@users.noreply.github.com>
2021-10-03 00:23:07 +0530

    Stack but proper approach

--- dsa3.cpp
+++ dsa3.cpp
@@ -1,48 +1,61 @@
-void printLevelOrderTreeFashion(node* root){
-    queue<node*>q;
-    q.push(root);
-    q.push(NULL);
-    
-    while(!q.empty()){
-        node* temp = q.front();
-        if(temp==NULL){
-            cout<<endl;
-            q.pop();
-            
-            if(!q.empty()){
-                q.push(NULL);
-            }
-        }
-        else{
-        q.pop();
-        cout<<temp->data<<" ";
-        if(temp->left){
-            q.push(temp->left);
-        }
-        if(temp->right){
-            q.push(temp->right);
-        }
-        
-        }
+
+//Using Arrays
+#include <iostream>
+using namespace std;
+
+int MAXSIZE = 10;
+int stack[10];
+int top = -1;
+
+
+bool isFull(){
+    return top==MAXSIZE-1;
 }
-    int knapsack(int W,Item k[],int n){
-    sort(k,k+n,comp);
-    
-    double final=0.0;
-    int cur=0;
-    
-    for(int i=0;i<n;i++){
-        if(cur+k[i].weight<=W){
-            cur = cur + k[i].weight;
-            final = final + k[i].value;
-        }
-        
-        // if there is still some capacity in the bucket, we take a fractio of the next item
-        else{
-            int rem = W-cur;
-            final = final + ((double)k[i].value/(double)k[i].weight)*(double)rem;
-            break;
-        }
+bool isEmpty(){
+    return top==-1;
+}
+void push(int a){
+    if(!isFull()){
+        top = top+1;
+        stack[top]=a;
     }
-    return final;
+    else
+        cout<<"Stack Overflow"<<endl;
 }
+void tops(){
+    cout<<stack[top]<<endl;
+}
+void display(){
+    for(int i=top;i>=0;i--){
+        cout<<stack[i]<<" ";
+    }
+    cout<<endl;
+}
+void pop(){
+    if(!isEmpty())
+        top=top-1;
+    else
+        cout<<"Stack isEmpty"<<endl;
+}
+
+
+int main()
+{
+    
+    push(1);
+    push(2);
+    push(3);
+    push(4);
+    push(5);
+    display();
+    pop();
+    display();
+    tops();
+    
+    display();
+    
+
+    return 0;
+}
+© 2021 GitHub, Inc.
+Terms
