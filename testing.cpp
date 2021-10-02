a26be4ecb46af310c3fa7ae4fab8e444f018987e
GeethaTallur <72148952+GeethaTallur@users.noreply.github.com>
2021-10-03 00:58:17 +0530

    deleted reduntant code.

--- testing.cpp
+++ testing.cpp
@@ -5,10 +5,6 @@ int main(){
  int a,b,c =0;
   char ans = 'a';
  string abc = "hello world";
- printLevelOrderNormal(root);
-    cout<<endl<<endl;
-    printLevelOrderTreeFashion(root);
-    cout<<endl<<endl;
-    printInOrder(root);
+ double f,g,v = 7.89;
 return 0;
 }
