1a5f1cf4be1dae286cd9e42b3c116e6b39f66525
ShankaVerma <72945733+ShankaVerma@users.noreply.github.com>
2021-10-02 13:10:55 +0530

    BST
    added BST code

--- dsa4.cpp
+++ dsa4.cpp
@@ -1,71 +1,49 @@
-#include <iostream>
-#include<climits>
+#include <bits/stdc++.h>
+
 using namespace std;
-int findMinVertex(bool *visited,int *weights,int n)
-{
-    int min=-1;
-    for(int i=0;i<n;i++)
-    {
-        if(!visited[i] && (min==-1 || weights[i]<weights[min]))
-        {
-            min=i;
-        }
+
+class node{
+    public:
+          int data;
+          node* left;
+          node* right;
+          
+          node(int d){
+              data=d;
+              left=NULL;
+              right=NULL;
+          }
+};
+
+node* buildBST(node* root,int data){
+    if(root==NULL){
+        return new node(data);
     }
-    return min;
+    else if(data<root->data){
+        root->left = buildBST(root->left,data);
+    }
+    else{
+        root->right = buildBST(root->right,data);
+    }
+    return root;
 }
-void prims(int **edges,int n)
-{
-    int *parent =new int[n];
-    int *weights =new int[n];
-    bool*visited =new bool[n];
+
+void printLevelOrderNormal(node* root){
+    queue<node*>q;
+    q.push(root);
     
-    for(int i=1;i<n;i++)
-    {
-        visited[i]=false;
-        weights[i]=INT_MAX;
-    }
-    parent[0]=-1;
-    weights[0]=0;
-    for(int i=0;i<n-1;i++)
-    {
-    int minVertex=findMinVertex(visited,weights,n);
-    visited[minVertex]=true;
-    for(int j=0;j<n;j++)
-    {
-        if(!visited[j] && edges[minVertex][j]!=0)
-        {
-            if(edges[minVertex][j] + weight[minVertex]<weights[j])
-            {
-                weights[j]=edges[minVertex][j] + weight[minVertex];
-            }
+    
+    while(!q.empty()){
+        node* temp = q.front();
+        cout<<temp->data<<" ";
+        q.pop();
+        
+        if(temp->left){
+            q.push(temp->left);
         }
-    }
-    }
-    for(int i=1;i<n;i++)
-           cout<<i<<" "<<i<<" "<<weights[i]<<endl;
-   
-}
-int main()
-{   int f,s,weight;
-    int n,e;
-    cin>>n>>e;
-    int** edges = new int*[n];
-    for(int i=0;i<n;i++)
-    {   
-       edges[i]=new int[n];
-       for(int j=0;j<n;j++)
-        {
-            edges[i][j]=0;
+        if(temp->right){
+            q.push(temp->right);
         }
+        
     }
-    
-    for(int j=0;j<e;j++)
-    {
-       cin>>f>>s>>weight;
-       edges[f][s]=weight;
-       edges[s][f]=weight;
-    }
-    cout<<endl;
-    prims(edges,n);
-    
 }
